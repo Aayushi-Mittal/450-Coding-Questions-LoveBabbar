d868cfbbcf632201557b900914d4d2958b175d79
Aayushi-Mittal <aayushimittal088@gmail.com>
2020-11-21 23:32:48 +0530

    Solution for 02-Max-Min-Elements.cpp

---
+++ 01-Arrays/02-Max-Min-Elements.cpp
@@ -0,0 +1,33 @@
+//Write a program to find maximum and minimum elements in an array.
+/*
+COMPLEXITY - O()
+SAMPLE INPUT
+6
+4 7 8 9 21 40 6 4 1
+OUTPUT
+MAX : 40
+MIN : 1
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i=0;
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int max=a[0], min=a[0];
+    for(i=1; i<n; i++)
+    {
+        if(a[i]>max)
+            max=a[i];
+        if(a[i]<min)
+            max=a[i];
+    }
+    cout<<"MAX : "<<max<<endl;
+    cout<<"MIN : "<<min<<endl;
+}

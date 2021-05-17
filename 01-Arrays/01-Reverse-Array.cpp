c008047ab9fe225bd36b3362b244ad370b8eb8c7
Aayushi Mittal <aayushimittal088@gmail.com>
2021-05-17 19:23:41 +0530

    Add Solution for  01-Reverse-Array.cpp

--- 01-Arrays/01-Reverse-Array.cpp
+++ 01-Arrays/01-Reverse-Array.cpp
@@ -1,15 +1,4 @@
 //Write a program to reverse an array or string.
-/*
-SAMPLE INPUT
-6
-4 7 a 8 3 b
-OUTPUT
-arr[6] : {4, 7, a, 8, 3, b, }
-revarr[6] : {b, 3, 8, a, 7, 4, }
-COMPLEXITY
-Time : O(n)
-Space : O(n)
-*/
 
 #include<iostream>
 using namespace std;
@@ -17,10 +6,12 @@ int main()
 {
     int n=100, i=0;
     char arr[n], revarr[n];
+    cout<<"Enter the size of the array: ";
     cin>>n;                 //size of array
+    cout<<"Enter "<<n<<" elements of the array: ";
     for(i=0; i<n; i++)
     {
-        cin>>arr[i];        //characters of array
+        cin>>arr[i];
     }
     cout<<"\narr["<<n<<"] : {";
     for(i=0; i<n; i++)
@@ -32,7 +23,25 @@ int main()
     cout<<"revarr["<<n<<"] : {";
     for(i=0; i<n; i++)
     {
-        cout<<revarr[i]<<", ";  //elements of reverse array
+        cout<<revarr[i]<<", ";
     }
     cout<<"}\n";
+    return 0;
 }
+
+/*
+
+Sample I/O
+
+Input:
+Enter the size of the array: 6
+Enter 6 elements of the array: 4 7 a 8 3 b
+
+Output:
+arr[6] : {4, 7, a, 8, 3, b, }
+revarr[6] : {b, 3, 8, a, 7, 4, }
+
+Time Complexity : O(n)
+Space Complexity : O(n)
+
+*/
